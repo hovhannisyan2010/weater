ef7c565255608cadbe9fe5e30af10366c3c2c383
hovhannisyan2010 <hovhannisyanjora2010@gmail.com>
2024-11-12 16:39:37 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const input = document.querySelector(".inp")
+  const [weater,setWeater] = useState({})
+  const [inp,setInp] = useState("")
+  function weaterSubmit(event) {
+      event.preventDefault()
+      setInp(event.target.input.value)    
+    }
+  useEffect(()=>{
+      fetch(`https://api.weatherapi.com/v1/current.json?key=e857cf09ed224ed4adf82811240211&q=${inp}&aqi=no%60`)
+      .then(res=>res.json())
+      .then(res=>setWeater(res))
+  },[inp])
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <form onSubmit={weaterSubmit} className='form'>
+      <input type='text' className='inp'  name='input'/>
+      <button type='submit' className='submit'>submit</button>
+      </form>
+      <div>
+        <h1>{weater.location?weater.location.name:null}</h1>
+        <h1>{weater.current?`${weater.current.temp_c}C`:null}</h1>
+        <h1>{weater.current?weater.current.condition.text:null}</h1>
+        <img src={weater.current?weater.current.condition.icon:null}/>
+        <img/>
+      </div>
     </div>
   );
 }
